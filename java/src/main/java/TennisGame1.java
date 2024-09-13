a25587c2b842d3e05015fc5dbe3de19ff78dd680
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-09-13 13:43:30 +0100

    Replace a == with a call to .equals

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -12,7 +12,7 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName.equals("player1"))
             player1Score += 1;
         else
             player2Score += 1;
